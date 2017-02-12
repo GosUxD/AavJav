4086ceef327fe91624e90c53450313da19ec66a1
Daniel <daniel_napster@hotmail.com>
2017-02-12 16:04:21 +0100

    First Push

---
+++ app/src/main/java/com/aavjaav/qd/aavjaav/Application.java
@@ -0,0 +1,16 @@
+package com.aavjaav.qd.aavjaav;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+public class Application extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Lato-Black.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+}
---
+++ app/src/main/java/com/aavjaav/qd/aavjaav/view/SplashActivity.java
@@ -0,0 +1,172 @@
+package com.aavjaav.qd.aavjaav.view;
+
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.aavjaav.qd.aavjaav.R;
+import com.chabbal.slidingdotsplash.SlidingSplashView;
+import com.xgc1986.parallaxPagerTransformer.ParallaxPagerTransformer;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        initUI();
+
+    }
+
+    private void initUI() {
+        final HorizontalScrollView scrollView = (HorizontalScrollView) findViewById(R.id.scroll_view);
+        final ImageView imageView = (ImageView) findViewById(R.id.background);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                int x = (int) ((mViewPager.getWidth() * position + positionOffsetPixels) * computeFactor());
+                scrollView.scrollTo(x, 0);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+
+            private float computeFactor() {
+                return (imageView.getWidth() - mViewPager.getWidth()) /
+                        (float)(mViewPager.getWidth() * (mViewPager.getAdapter().getCount() - 1));
+            }
+        });
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabDots);
+        tabLayout.setupWithViewPager(mViewPager, true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+        private static final String ARG_SECTION_NUMBER = "section_number";
+        private TextView mIntroText;
+        private CharSequence mIntroValue;
+
+        public PlaceholderFragment() {
+        }
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_splash, container, false);
+            mIntroText = (TextView) rootView.findViewById(R.id.fragment_splash_intro_text);
+
+            int introNumber = getArguments().getInt(ARG_SECTION_NUMBER);
+            switch (introNumber) {
+                case 1:
+                    mIntroValue =  getResources().getText(R.string.intro_text_1);
+                    break;
+                case 2:
+                    mIntroValue =  getResources().getText(R.string.intro_text_2);
+                    break;
+                case 3:
+                    mIntroValue =  getResources().getText(R.string.intro_text_3);
+                    break;
+            }
+
+            mIntroText.setText(mIntroValue);
+            return rootView;
+        }
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "";
+                case 1:
+                    return "";
+                case 2:
+                    return "";
+            }
+            return null;
+        }
+    }
+}
